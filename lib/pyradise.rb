19f3b011cf90c1bd0c0aab66a32c56aca69c9e04
Marcos Augusto <x@nofxx.com>
2009-12-30 05:12:48 -0200

    fix command line

--- lib/pyradise.rb
+++ lib/pyradise.rb
@@ -4,31 +4,35 @@ require 'sequel'
 module Pyradise
 
   HOME = ENV['HOME'] + "/.pyradise"
-  #TODO: def init
   DB = Sequel.connect("sqlite://#{HOME}/py.sqlite3")
+  Defaults = {:rate => 1, :tax => 1.3, :order => :price, :lang => :en_us}
+  Options  = Defaults.merge(YAML.load(File.new(HOME + "/conf.yml")))
+  
   def locale
     `locale | grep LANG`.scan(/LANG=(.*)\./)[0][0].downcase rescue "en_us"
   end
 
-  unless File.exists? HOME
-    FileUtils.mkdir_p HOME
-    conf = open(HOME + "/conf.yml", "wb")
-    conf.write(":rate: 1.8  # dollar conversion rate\n")
-    conf.write(":tax:  1.3  # transport tax\n")
-    conf.write(":lang: #{locale}  # program language\n")
-    conf.close
-  end
+  def self.start
+    unless File.exists? HOME
+      FileUtils.mkdir_p HOME
+      conf = open(HOME + "/conf.yml", "wb")
+      conf.write(":rate: 1.8  # dollar conversion rate\n")
+      conf.write(":tax:  1.3  # transport tax\n")
+      conf.write(":lang: #{locale}  # program language\n")
+      conf.close
+    end
 
- # require 'pyradise/stat'
-  unless DB.table_exists? :products
-    require 'pyradise/migrate'
-    CreatePyradise.apply DB, :up
-  end
-  Defaults = {:rate => 1, :tax => 1.3, :order => :price, :lang => :en_us}
-  Options = Defaults.merge(YAML.load(File.new(HOME + "/conf.yml")))
+    # require 'pyradise/stat'
+    unless DB.table_exists? :products
+      require 'pyradise/migrate'
+      CreatePyradise.apply DB, :up
+    end
 
-  require 'pyradise/product'
-  require 'pyradise/i18n'
-  require 'pyradise/cli'
+    require 'pyradise/product'
+    require 'pyradise/i18n'
+    require 'pyradise/cli'
+  end
 
 end
+
+Pyradise.start
--- pyradise.gemspec
+++ pyradise.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{pyradise}
-  s.version = "0.3.1"
+  s.version = "0.3.2"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Marcos Piccinini"]
-  s.date = %q{2009-12-28}
+  s.date = %q{2009-12-30}
   s.default_executable = %q{pyradise}
   s.email = %q{x@nofxx.com}
   s.executables = ["pyradise"]
@@ -43,9 +43,9 @@ Gem::Specification.new do |s|
   s.rubygems_version = %q{1.3.5}
   s.summary = %q{Paraguay gem!}
   s.test_files = [
-    "spec/spec_helper.rb",
+    "spec/pyradise/product_spec.rb",
      "spec/pyradise_spec.rb",
-     "spec/pyradise/product_spec.rb"
+     "spec/spec_helper.rb"
   ]
 
   if s.respond_to? :specification_version then
