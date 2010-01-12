b0f050a1beded95da83221680b023bf26af46e4e
Marcos Piccinini <x@nofxx.com>
2010-01-12 14:04:46 -0200

    fix config files missing prob. closes #3

--- lib/pyradise.rb
+++ lib/pyradise.rb
@@ -1,27 +1,24 @@
 require 'open-uri'
 require 'sequel'
 
+HOME = ENV['HOME'] + "/.pyradise"
+unless File.exists? HOME
+  FileUtils.mkdir_p HOME
+  locale = `locale | grep LANG`.scan(/LANG=(.*)\./)[0][0].downcase rescue "en_us"
+  conf = open(HOME + "/conf.yml", "wb")
+  conf.write(":rate: 1.8  # dollar conversion rate\n")
+  conf.write(":tax:  1.3  # transport tax\n")
+  conf.write(":lang: %s  # program language\n" % locale )
+  conf.close
+end
+
 module Pyradise
 
-  HOME = ENV['HOME'] + "/.pyradise"
   DB = Sequel.connect("sqlite://#{HOME}/py.sqlite3")
   Defaults = {:rate => 1, :tax => 1.3, :order => :price, :lang => :en_us}
   Options  = Defaults.merge(YAML.load(File.new(HOME + "/conf.yml")))
-  
-  def locale
-    `locale | grep LANG`.scan(/LANG=(.*)\./)[0][0].downcase rescue "en_us"
-  end
 
   def self.start
-    unless File.exists? HOME
-      FileUtils.mkdir_p HOME
-      conf = open(HOME + "/conf.yml", "wb")
-      conf.write(":rate: 1.8  # dollar conversion rate\n")
-      conf.write(":tax:  1.3  # transport tax\n")
-      conf.write(":lang: #{locale}  # program language\n")
-      conf.close
-    end
-
     # require 'pyradise/stat'
     unless DB.table_exists? :products
       require 'pyradise/migrate'
